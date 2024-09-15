005135f77f4a2d96f903d556a610ca2ffcc11a87
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-15 18:20:37 +0100

    upadte

--- components/forms/AddProduct.tsx
+++ components/forms/AddProduct.tsx
@@ -174,122 +174,127 @@ const AddProduct: React.FC<AddProductProps> = ({
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        {error && (
-          <p className="text-center py-3 text-xs text-red-500">{error}</p>
-        )}
-        <div className="grid gap-5">
-          {/* Title Input */}
-          <div className="grid gap-1">
-            <label
-              htmlFor="title"
-              className="text-xs md:text-sm font-light pl-2 text-[#B4C6EE]"
-            >
-              Enter title
-            </label>
-            <Input
-              type="text"
-              id="title"
-              placeholder="How to make money online"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className="bg-[#27344D] focus:border-[#3371FF] placeholder:text-xs text-xs md:text-sm md:placeholder:text-sm placeholder:font-light placeholder:opacity-70 border-[#2E3D5B] h-[52px] rounded-[8px]"
-            />
-          </div>
-
-          {/* Link Input */}
-          <div className="grid gap-1">
-            <label
-              htmlFor="link"
-              className="text-xs md:text-sm font-light pl-2 text-[#B4C6EE]"
-            >
-              Enter item URL
-            </label>
-            <Input
-              type="text"
-              id="link"
-              placeholder="https://www.example.com"
-              value={link}
-              onChange={(e) => setLink(e.target.value)}
-              className="bg-[#27344D] focus:border-[#3371FF] placeholder:text-xs text-xs md:text-sm md:placeholder:text-sm placeholder:font-light placeholder:opacity-70 border-[#2E3D5B] h-[52px] rounded-[8px]"
-            />
-          </div>
-
-          {/* File Upload Section */}
-          <div className="grid gap-3">
-            <label className="text-xs md:text-sm font-light pl-2 text-[#B4C6EE]">
-              Banner image
-            </label>
-            <div className="relative">
-              {/* Loading spinner */}
-              {uploading && (
-                <div className="absolute z-50 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] flex items-center justify-center">
-                  <svg
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 50 50"
-                    width="50px"
-                    height="50px"
-                  >
-                    <path
-                      fill="none"
-                      stroke="#2828D1"
-                      strokeWidth="4"
-                      strokeMiterlimit="10"
-                      d="M25,5 A20,20 0 1,1 24.999,5"
-                      strokeDasharray="31.4 31.4"
-                      strokeLinecap="round"
-                      transform="rotate(225 25 25)"
-                    >
-                      <animateTransform
-                        attributeName="transform"
-                        type="rotate"
-                        from="0 25 25"
-                        to="360 25 25"
-                        dur="1s"
-                        repeatCount="indefinite"
-                      />
-                    </path>
-                  </svg>
-                </div>
+        {type === "add" ||
+          ("update" && (
+            <>
+              {error && (
+                <p className="text-center py-3 text-xs text-red-500">{error}</p>
               )}
-
-              {/* Uploaded Image Preview */}
-              {imageUrl ? (
-                <div className="relative">
-                  <img
-                    src={imageUrl}
-                    alt="Uploaded banner"
-                    className="rounded-[10px] w-full min-h-[150px] max-h-[200px]"
+              <div className="grid gap-5">
+                {/* Title Input */}
+                <div className="grid gap-1">
+                  <label
+                    htmlFor="title"
+                    className="text-xs md:text-sm font-light pl-2 text-[#B4C6EE]"
+                  >
+                    Enter title
+                  </label>
+                  <Input
+                    type="text"
+                    id="title"
+                    placeholder="How to make money online"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="bg-[#27344D] focus:border-[#3371FF] placeholder:text-xs text-xs md:text-sm md:placeholder:text-sm placeholder:font-light placeholder:opacity-70 border-[#2E3D5B] h-[52px] rounded-[8px]"
                   />
                 </div>
-              ) : (
-                <div className="bg-[#27344D] min-h-[150px] relative rounded-[10px]"></div>
-              )}
 
-              {/* Upload Button */}
-              <button
-                type="button"
-                onClick={() => {
-                  const fileInput = document.getElementById(
-                    "file-upload"
-                  ) as HTMLInputElement | null;
-                  if (fileInput) {
-                    fileInput.click(); // Only call click if the element is not null
-                  }
-                }}
-                className="absolute right-5 top-5 flex items-center justify-center w-[56px] h-[56px] rounded-full bg-[#151E2F]/95"
-              >
-                <input
-                  type="file"
-                  id="file-upload"
-                  className="hidden"
-                  onChange={handleFileChange}
-                />
-                <UploadCloudIcon />
-              </button>
-            </div>
-          </div>
-        </div>
+                {/* Link Input */}
+                <div className="grid gap-1">
+                  <label
+                    htmlFor="link"
+                    className="text-xs md:text-sm font-light pl-2 text-[#B4C6EE]"
+                  >
+                    Enter item URL
+                  </label>
+                  <Input
+                    type="text"
+                    id="link"
+                    placeholder="https://www.example.com"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
+                    className="bg-[#27344D] focus:border-[#3371FF] placeholder:text-xs text-xs md:text-sm md:placeholder:text-sm placeholder:font-light placeholder:opacity-70 border-[#2E3D5B] h-[52px] rounded-[8px]"
+                  />
+                </div>
+
+                {/* File Upload Section */}
+                <div className="grid gap-3">
+                  <label className="text-xs md:text-sm font-light pl-2 text-[#B4C6EE]">
+                    Banner image
+                  </label>
+                  <div className="relative">
+                    {/* Loading spinner */}
+                    {uploading && (
+                      <div className="absolute z-50 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] flex items-center justify-center">
+                        <svg
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 50 50"
+                          width="50px"
+                          height="50px"
+                        >
+                          <path
+                            fill="none"
+                            stroke="#2828D1"
+                            strokeWidth="4"
+                            strokeMiterlimit="10"
+                            d="M25,5 A20,20 0 1,1 24.999,5"
+                            strokeDasharray="31.4 31.4"
+                            strokeLinecap="round"
+                            transform="rotate(225 25 25)"
+                          >
+                            <animateTransform
+                              attributeName="transform"
+                              type="rotate"
+                              from="0 25 25"
+                              to="360 25 25"
+                              dur="1s"
+                              repeatCount="indefinite"
+                            />
+                          </path>
+                        </svg>
+                      </div>
+                    )}
+
+                    {/* Uploaded Image Preview */}
+                    {imageUrl ? (
+                      <div className="relative">
+                        <img
+                          src={imageUrl}
+                          alt="Uploaded banner"
+                          className="rounded-[10px] w-full min-h-[150px] max-h-[200px]"
+                        />
+                      </div>
+                    ) : (
+                      <div className="bg-[#27344D] min-h-[150px] relative rounded-[10px]"></div>
+                    )}
+
+                    {/* Upload Button */}
+                    <button
+                      type="button"
+                      onClick={() => {
+                        const fileInput = document.getElementById(
+                          "file-upload"
+                        ) as HTMLInputElement | null;
+                        if (fileInput) {
+                          fileInput.click(); // Only call click if the element is not null
+                        }
+                      }}
+                      className="absolute right-5 top-5 flex items-center justify-center w-[56px] h-[56px] rounded-full bg-[#151E2F]/95"
+                    >
+                      <input
+                        type="file"
+                        id="file-upload"
+                        className="hidden"
+                        onChange={handleFileChange}
+                      />
+                      <UploadCloudIcon />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
 
         {/* Submit Button */}
         <Button
